fc51a43540c97d9107b13dd2cafe514ae72860e0
Jeldrik Hanschke <admin@jhanschke.de>
2022-12-07 21:34:56 +0100

    ember install ember-bootstrap (twice)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 5,
+      importBootstrapCSS: true
+    }
   });
 
   /*
